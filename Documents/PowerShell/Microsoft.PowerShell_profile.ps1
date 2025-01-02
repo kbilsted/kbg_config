f8941825b2bd27143e4529fdfd777153cb0c4616
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2025-01-02 23:28:32 +0100

    bin

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,23 @@
 # this is the configuration for powershell 7
 
+" o s - cd"
 function s(){cd ..}
+
+" o x - exit"
 function x(){exit }
 
+" o k - keyboard help"
 function k()
 {
-  cd C:\util\keymapper-4.9.1-win64
+  cd ~\kbgstuff\keymapper-4.9.1-win64
   ".\keymapperd.exe"
   & .\keymapperd.exe
   
-  ".\keymapper.exe -u -c  C:\src\StatusWindow\keymapper.conf "
-  & .\keymapper.exe -u -c  C:\src\StatusWindow\keymapper.conf 
+  ".\keymapper.exe -u -c .\keymapper.conf"
+  & .\keymapper.exe -u -c .\keymapper.conf
 }
 
+" o kk - kill keyboard help"
 function kk()
 {
   "taskkill /IM keymapper.exe"
@@ -22,4 +27,9 @@ function kk()
   taskkill /IM keymapperd.exe
 }
 
-cd c:\src\
+# cd c:\src\
+
+
+
+# improved autocompletion
+Import-Module PSReadLine
